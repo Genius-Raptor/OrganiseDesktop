a735bec31dc0deb56ca8598fd99bdacc141ea3a5
Genius <Velociraptor.rex@hotmail.com>
2024-08-05 15:06:14 +0500

    Moved Tkinter UI to Web App using flask. Modernised UI with bootstrap. Decoupled front and backend.

--- organise_desktop/Clean.py
+++ organise_desktop/Clean.py
@@ -1,8 +1,11 @@
 import sys
 import json
 import os
-from cronController import  schedule_end, schedule_start
-from organiseDesktop import undo, organise_desktop
+from cronController import schedule_end, schedule_start
+from organiseDesktop import undo as undoo, organise_desktop
+from flask import Flask, app, render_template, redirect
+
+app = Flask(__name__)
 
 if sys.version_info >= (3,):
     from tkinter import *
@@ -15,15 +18,33 @@ pwd = os.path.dirname(os.path.abspath(__file__))
 Extensions = json.load(open(pwd+'/Extension.json', 'r'))
 folders = [x for x in Extensions]
 
+
+@app.route('/undo', methods=['POST'])
+def undo():
+    undoo()
+    return redirect('/')
+
+
+@app.route('/schedule-end', methods=['POST'])
+def on_schedule_end():
+    schedule_end()
+    return redirect('/')
+
+
 class App(Frame):
     """define the GUI"""
-    def clean(self):
+
+    @app.route('/clean', methods=['POST'])
+    def clean():
         checked_extensions = {}
         for x in folders:
             checked_extensions[x] = Extensions[x]
         organise_desktop(checked_extensions)
-        tkMessageBox.showinfo('Complete', 'Desktop clean finished.')
+        return redirect('/')
+        # tkMessageBox.showinfo('Complete', 'Desktop clean finished.')
+        # TODO: Show message box
 
+    @app.route('/quit', methods=['POST'])
     def quit_all(self):
         sys.exit(0)
 
@@ -34,59 +55,73 @@ class App(Frame):
         else:
             folders.append(item)
 
-    def on_schedule_start(self):
+    @app.route('/schedule', methods=['POST'])
+    def on_schedule_start():
         schedule_start(folders)
+        return redirect('/')
+    # NOT NEEDED
+    # def make_checkbutton(self, text):
+    #     cb = Checkbutton(self, text=text, command=lambda: self.check(text))
+    #     cb.select()
+    #     cb.pack({'side': 'top'})
+    #     return cb
 
-    def make_checkbutton(self, text):
-        cb = Checkbutton(self, text=text, command=lambda: self.check(text))
-        cb.select()
-        cb.pack({'side': 'top'})
-        return cb
+    # NOT NEEDED
+    # def make_button(self, text, command):
+    #     btn = Button(self, text=text, command=command)
+    #     btn.pack({'side': 'left'})
+    #     return btn
 
-    def make_button(self, text, command):
-        btn = Button(self, text=text, command=command)
-        btn.pack({'side': 'left'})
-        return btn
+    # NOT NEEDED
+    # def create(self):
+    #     self.winfo_toplevel().title('Desktop Cleaner')
 
-    def create(self):
-        self.winfo_toplevel().title('Desktop Cleaner')
+    #     for ext in sorted(Extensions.keys()):
+    #         self.make_checkbutton(ext)
 
-        for ext in sorted(Extensions.keys()):
-            self.make_checkbutton(ext)
+    #     # buttons and their respective functions
+    #     buttons = {'Clean': self.clean,
+    #                'Exit': self.quit_all,
+    #                'Undo': undo,
+    #                'Schedule': self.on_schedule_start,
+    #                'Remove\nSchedule': schedule_end
+    #                }
 
-        # buttons and their respective functions
-        buttons = {'Clean': self.clean,
-                   'Exit': self.quit_all,
-                   'Undo': undo,
-                   'Schedule': self.on_schedule_start,
-                   'Remove\nSchedule': schedule_end
-                   }
-
-        for key in buttons:
-            self.make_button(key, buttons[key])
+    #     for key in buttons:
+    #         self.make_button(key, buttons[key])
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
         self.create()
 
+
+@app.route('/')
 def main():
-    root = Tk()
-    # root.resizable = False            # commenting this approach and applying the below one.
-    root.resizable(FALSE,FALSE)         # To make the application's size constant and restore button in windows as greyed out(with width=350 and height=330 as mentioned below)
-    root.minsize(width=350, height=330)
-    root.maxsize(width=350, height=330)
+    # OLD TKINTER UI
 
-    '''Logic to launch the app in center - start'''
-    positionRight = int(root.winfo_screenwidth() / 2 - 330 / 2) #considering width=330
-    positionDown = int(root.winfo_screenheight() / 2 - 350 / 2) #considering height=350
-    root.geometry("+{}+{}".format(positionRight, positionDown))
-    '''Logic to launch the app in center - end'''
+    # root = Tk()
+    # # root.resizable = False            # commenting this approach and applying the below one.
+    # # To make the application's size constant and restore button in windows as greyed out(with width=350 and height=330 as mentioned below)
+    # root.resizable(FALSE, FALSE)
+    # root.minsize(width=350, height=330)
+    # root.maxsize(width=350, height=330)
 
-    app = App(root)
-    root.protocol('WM_DELETE_WINDOW', app.quit_all)
-    app.mainloop()
-    root.destroy()
+    # '''Logic to launch the app in center - start'''
+    # positionRight = int(root.winfo_screenwidth() / 2 -
+    #                     330 / 2)  # considering width=330
+    # positionDown = int(root.winfo_screenheight() / 2 -
+    #                    350 / 2)  # considering height=350
+    # root.geometry("+{}+{}".format(positionRight, positionDown))
+    # '''Logic to launch the app in center - end'''
+
+    # app = App(root)
+    # root.protocol('WM_DELETE_WINDOW', app.quit_all)
+    # app.mainloop()
+    # root.destroy()
+
+    return render_template('index.html', extensions=Extensions)
+
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
